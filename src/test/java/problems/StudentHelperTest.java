8fc90920b4798f604a5528275e20cfc8f29dc76a
Yash Sharma <yash.sharma@olx.in>
2025-04-12 12:17:21 +0530

    Add Qualify tests clean code

--- src/test/java/problems/StudentHelperTest.java
+++ src/test/java/problems/StudentHelperTest.java
@@ -69,28 +69,34 @@ public class StudentHelperTest {
 		assertEquals("B", helper.getGrade(95, true));
 	}
 
+	// Test For Qualify Component
 	@Test
-	public void testWillQualifyForQuiz() {
-		assertEquals("NO",helper.willQualifyForQuiz(15, 25, false));
-		assertEquals("NO",helper.willQualifyForQuiz(20, 20, false));
-		assertEquals("MAYBE",helper.willQualifyForQuiz(21, 21, false));
-		assertEquals("MAYBE",helper.willQualifyForQuiz(25, 25, false));
-		assertEquals("MAYBE",helper.willQualifyForQuiz(79, 79, false));
-		assertEquals("YES",helper.willQualifyForQuiz(80, 30, false));
-		assertEquals("YES",helper.willQualifyForQuiz(85, 30, false));
-		assertEquals("YES",helper.willQualifyForQuiz(30, 90, false));
-		
-		assertEquals("NO",helper.willQualifyForQuiz(15, 25, true));
-		assertEquals("NO",helper.willQualifyForQuiz(20, 20, true));
-		assertEquals("NO",helper.willQualifyForQuiz(21, 21, true));
-		assertEquals("NO",helper.willQualifyForQuiz(25, 25, true));
-		assertEquals("MAYBE",helper.willQualifyForQuiz(27, 34, true));
-		assertEquals("MAYBE",helper.willQualifyForQuiz(79, 79, true));
-		assertEquals("MAYBE",helper.willQualifyForQuiz(80, 30, true));
-		assertEquals("MAYBE",helper.willQualifyForQuiz(80, 84, true));
-		assertEquals("YES",helper.willQualifyForQuiz(85, 30, true));
-		assertEquals("YES",helper.willQualifyForQuiz(30, 90, true));
-
+	public void shouldReturnNo_WhenScoresBelowThreshold() {
+		assertEquals("NO", helper.willQualifyForQuiz(15, 25, false));
+		assertEquals("NO", helper.willQualifyForQuiz(20, 20, false));
+		assertEquals("NO", helper.willQualifyForQuiz(15, 25, true));
+		assertEquals("NO", helper.willQualifyForQuiz(20, 20, true));
+		assertEquals("NO", helper.willQualifyForQuiz(21, 21, true));
+		assertEquals("NO", helper.willQualifyForQuiz(25, 25, true));
 	}
 
+	@Test
+	public void shouldReturnMaybe_WhenScoresInMidRange() {
+		assertEquals("MAYBE", helper.willQualifyForQuiz(21, 21, false));
+		assertEquals("MAYBE", helper.willQualifyForQuiz(25, 25, false));
+		assertEquals("MAYBE", helper.willQualifyForQuiz(79, 79, false));
+		assertEquals("MAYBE", helper.willQualifyForQuiz(27, 34, true));
+		assertEquals("MAYBE", helper.willQualifyForQuiz(79, 79, true));
+		assertEquals("MAYBE", helper.willQualifyForQuiz(80, 30, true));
+		assertEquals("MAYBE", helper.willQualifyForQuiz(80, 84, true));
+	}
+
+	@Test
+	public void shouldReturnYes_WhenScoresAboveThreshold() {
+		assertEquals("YES", helper.willQualifyForQuiz(80, 30, false));
+		assertEquals("YES", helper.willQualifyForQuiz(85, 30, false));
+		assertEquals("YES", helper.willQualifyForQuiz(30, 90, false));
+		assertEquals("YES", helper.willQualifyForQuiz(85, 30, true));
+		assertEquals("YES", helper.willQualifyForQuiz(30, 90, true));
+	}
 }
