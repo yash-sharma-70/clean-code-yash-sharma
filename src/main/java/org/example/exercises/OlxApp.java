fdec027a63368496b6f76b1a0c3eee31befc150d
Yash Sharma <yash.sharma@olx.in>
2025-04-12 12:02:12 +0530

    Add exercises OlxApp clean code

--- src/main/java/org/example/exercises/OlxApp.java
+++ src/main/java/org/example/exercises/OlxApp.java
@@ -3,7 +3,12 @@ package org.example.exercises;
 import java.util.Arrays;
 
 public class OlxApp {
-	
+	private static final int MAX_QUALITY = 50;
+	private static final int MIN_QUALITY = 0;
+	private static final String AGED_WINE = "Aged Wine";
+	private static final String BACKSTAGE_PASS = "ColdPlay concert passes";
+	private static final String LEGENDARY_ITEM = "PokemonGo";
+
 	public Item[] items;
 
 	public OlxApp(Item[] items) {
@@ -12,76 +17,70 @@ public class OlxApp {
 
 	public static void main(String[] args) {
 
-		Item[] items = new Item[] { 
-							new Item("Mangoes", 10, 20)
-					   };
+		Item[] items = new Item[]{
+				new Item("Mangoes", 10, 20)
+		};
 
 		OlxApp app = new OlxApp(items);
-
 		app.updateQuality();
-
 		System.out.println(app);
-
 	}
 
 	public void updateQuality() {
-		for (int i = 0; i < items.length; i++) {
-			if (items[i].name != "Aged Wine" && items[i].name != "ColdPlay concert passes") {
-				if (items[i].quality > 0) {
-					if (items[i].name != "PokemonGo") {
-						items[i].quality = items[i].quality - 1;
-					}
-				}
+		for (Item item : items) {
+			if (!isSpecialItem(item)) {
+				decreaseQuality(item);
 			} else {
-				// This part handles the items for which quality can increase
-				// "ColdPlay concert passes" and "Aged Wine"
+				increaseQuality(item);
+			}
 
-				if (items[i].quality < 50) {
-					items[i].quality = items[i].quality + 1;
+			if (!isLegendary(item)) {
+				item.sellIn--;
+			}
+			if (item.sellIn < 0) {
+				handleExpiredItem(item);
+			}
+		}
+	}
 
-					if (items[i].name == "ColdPlay concert passes") {
-						if (items[i].sellIn < 11) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
+	private boolean isSpecialItem(Item item) {
+		return AGED_WINE.equals(item.name) || BACKSTAGE_PASS.equals(item.name);
+	}
 
-						if (items[i].sellIn < 6) {
-							if (items[i].quality < 50) {
-								items[i].quality = items[i].quality + 1;
-							}
-						}
-					}
+	private boolean isLegendary(Item item) {
+		return LEGENDARY_ITEM.equals(item.name);
+	}
+
+	private void decreaseQuality(Item item) {
+		if (item.quality > MIN_QUALITY && !isLegendary(item)) {
+			item.quality--;
+		}
+	}
+
+	private void increaseQuality(Item item) {
+		if (item.quality < MAX_QUALITY) {
+			item.quality++;
+
+			if (BACKSTAGE_PASS.equals(item.name)) {
+				if (item.sellIn < 11 && item.quality < MAX_QUALITY) {
+					item.quality++;
+				}
+				if (item.sellIn < 6 && item.quality < MAX_QUALITY) {
+					item.quality++;
 				}
 			}
+		}
+	}
 
-			// Everything except for Sulfuras the sellIn Decreases
-			if (items[i].name != "PokemonGo") {
-				items[i].sellIn = items[i].sellIn - 1;
+	private void handleExpiredItem(Item item) {
+		if (AGED_WINE.equals(item.name)) {
+			if (item.quality < MAX_QUALITY) {
+				item.quality++;
 			}
-
-			if (items[i].sellIn < 0) {
-				if (items[i].name != "Aged Wine") {
-
-					if (items[i].name != "ColdPlay concert passes") {
-						if (items[i].quality > 0) {
-							if (items[i].name != "PokemonGo") {
-								items[i].quality = items[i].quality - 1;
-							}
-						}
-					} else {
-						// For Backstage passes with sellin less than zero
-						// quality is set to zero
-						items[i].quality = 0;
-					}
-				} else {
-					// For Aged Wine below 50 quality increases actually by 2
-					// In the previous line
-					if (items[i].quality < 50) {
-						items[i].quality = items[i].quality + 1;
-					}
-				}
-			}
+		} else if (BACKSTAGE_PASS.equals(item.name)) {
+			item.quality = 0;
+		} else {
+			decreaseQuality(item);
 		}
 	}
 
@@ -89,5 +88,4 @@ public class OlxApp {
 	public String toString() {
 		return Arrays.toString(items);
 	}
-
 }
